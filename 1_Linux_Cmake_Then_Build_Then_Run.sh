db3fe392e5d62d22c9ac75cdc46db13f4be0161c
Michael Honey <michael.honey@upower.com>
2023-08-01 21:02:56 -0700

    Fixed compilation issue on Jetson Orin

--- 1_Linux_Cmake_Then_Build_Then_Run.sh
+++ 1_Linux_Cmake_Then_Build_Then_Run.sh
@@ -2,5 +2,5 @@ mkdir -p Saves/
 mkdir -p build
 rm -f build/Jets_2D
 cd build/
-cmake -GNinja -DCMAKE_BUILD_TYPE="Debug" -DARCH_61=1 -S .. -B .
+cmake -GNinja -DCMAKE_BUILD_TYPE="Debug" -DARCH_87=1 -S .. -B .
 ninja && cd .. && build/Jets_2D
--- 3_Linux_Cmake_Then_Build_Then_Not_Run.sh
+++ 3_Linux_Cmake_Then_Build_Then_Not_Run.sh
@@ -2,6 +2,6 @@ mkdir -p Saves/
 mkdir -p build
 rm -f build/Jets_2D
 cd build/
-cmake -GNinja -DCMAKE_BUILD_TYPE="Debug" -DARCH_61=1 -S .. -B .
+cmake -GNinja -DCMAKE_BUILD_TYPE="Debug" -DARCH_87=1 -S .. -B .
 ninja
 cd ..
