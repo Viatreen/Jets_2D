e11328174ef28e8aba2e1877fefa411cbe2d16ba
Viatreen <mick.honey@gmail.com>
2024-01-14 23:13:18 -0500

    Updated 1_Linux_Cmake_Then_Build_Then_Run.sh to use make instead of ninja

--- 1_Linux_Cmake_Then_Build_Then_Run.sh
+++ 1_Linux_Cmake_Then_Build_Then_Run.sh
@@ -3,5 +3,5 @@ mkdir -p Saves/
 mkdir -p build
 rm -f build/Jets_2D
 cd build/
-cmake -GNinja -DCMAKE_BUILD_TYPE="Release" -DARCH_61=1 -S .. -B .
-ninja && cd .. && build/Jets_2D
+cmake -DCMAKE_BUILD_TYPE="Release" -DARCH_61=1 -S .. -B .
+make && cd .. && build/Jets_2D
